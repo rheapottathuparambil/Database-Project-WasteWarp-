294173b74c864ad6dd57ae90baf2a23007355196
Rhea Pottathuparambil <52354140+rheapottathuparambil@users.noreply.github.com>
2020-11-24 16:12:49 -0600

    Create PHP Scripts (Web Interface) - part4.php

---
+++ PHP Scripts (Web Interface) - part4.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title> Display Details of a Energy Plant</title>
+    </head>
+    <body>
+        <div align="center">
+        <form action="display_controller.php" method="POST">
+            <fieldset>
+                <legend> Enter Details to Display</legend>
+                <label tid ="tidlabel" for="tid"> Trash ID: </label>
+                    <input type="text" name="tid"  /> <br/> <br/>
+                    <input id="button" type="submit" name="submit" />
+            </fieldset> 
+        </form>
+        </div>
+    </body>
+</html>
