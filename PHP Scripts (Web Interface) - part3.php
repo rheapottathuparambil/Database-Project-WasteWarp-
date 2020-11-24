e3db56648c9ef1376f5308d0f816645232098b75
Rhea Pottathuparambil <52354140+rheapottathuparambil@users.noreply.github.com>
2020-11-24 16:12:06 -0600

    Create PHP Scripts (Web Interface) - part3.php

---
+++ PHP Scripts (Web Interface) - part3.php
@@ -0,0 +1,22 @@
+<!--
+    Pranav Bhandari
+    Rhea Pottathuparambil
+-->
+
+<html>
+    <head>
+        <title> Display Details of Trash</title>
+    </head>
+    <body>
+        <div align="center">
+        <form action="update_controller.php" method="POST">
+            <fieldset>
+                <legend> Enter Details to Update</legend>
+                    <label tid ="tidlabel" for="tid"> Trash ID: </label>
+                    <input type="text" name="tid"  /> <br/> <br/>
+                    <input id="button" type="submit" name="submit" />
+            </fieldset> 
+        </form>
+        </div>
+    </body>
+</html>
